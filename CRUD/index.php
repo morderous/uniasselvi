1a2e98b958a31b7d1e7943b4af302c01e5fa7115
Lucas Hubes <lucashubes@gmail.com>
2020-01-08 23:04:59 -0300

    implementando componentes

--- CRUD/index.php
+++ CRUD/index.php
@@ -1,3 +1,8 @@
+<?php
+
+require_once ("php/component.php");
+
+?>
 <!doctype html>
 <html lang="pt">
 <head>
@@ -5,7 +10,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Exercicio UNIASSELVI</title>
+    <title>Loja UNIASSELVI</title>
 
     <script src="https://kit.fontawesome.com/5d48a46d27.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
@@ -16,7 +21,18 @@
 
 <main>
     <div class="container text-center">
-        <h1 class="py-4 bg-dark text-light rounded"><i class="fas fa-book-reader"></i> Loja Uniasselvi</h1>
+        <h1 class="py-4 bg-dark text-light rounded"><i class="fas fa-book-reader"></i> Cliente</h1>
+
+        <div class="d-flex justify-content-center">
+            <form action=""method="post" class="w-50">
+                <div class="py-2">
+                    <?php inputElement("<i class=\"fas fa-fingerprint\"></i>", "ID Cliente", "id_cliente", ""); ?>
+                </div>
+                <div class="pt-2">
+                    <?php inputElement("<i class=\"fas fa-id-card-alt\"></i>", "Nome do Cliente", "nome_cliente", ""); ?>
+                </div>
+            </form>
+        </div>
     </div>
 </main>
 
