984b13d434834680e8db54398e5282b1ac7f0da5
Lucas Hubes <lucashubes@gmail.com>
2020-01-10 14:01:19 -0300

    index page ready

--- CRUD/nav.php
+++ CRUD/nav.php
@@ -2,14 +2,14 @@
 
 ?>
 <nav class="navbar navbar-expand-lg navbar-light bg-dark text-white-50">
-    <a class="navbar-brand" href="#">Uniasselvi</a>
+    <a class="navbar-brand" href="https://portal.uniasselvi.com.br/">Uniasselvi</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarText">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="cliente.php">Cliente</a>
